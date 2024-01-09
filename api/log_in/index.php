439f89259c2014551dc700e46e5e37a96b9d3089
Fang0105 <nody0105@gmail.com>
2024-01-09 23:50:50 +0800

    prevent SQL injection

--- api/log_in/index.php
+++ api/log_in/index.php
@@ -15,9 +15,12 @@
     $name = $_POST["name"];
     $password = $_POST["password"];
 
-    $data = array();    
-    $sql = "SELECT * FROM admin WHERE name='{$name}' and password='{$password}';";
-    $result = mysqli_query($link, $sql);
+    $sql = "SELECT * FROM admin WHERE name=? AND password=?";
+    $stmt = mysqli_prepare($link, $sql);
+    mysqli_stmt_bind_param($stmt, "ss", $name, $password);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+
     if(mysqli_num_rows($result)>0){
         echo '<script>window.alert("登入成功")</script>';
         echo '<script>document.location.href="../../admin_control/index.html"</script>';
